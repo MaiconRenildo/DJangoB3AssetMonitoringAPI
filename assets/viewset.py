4f5be94c9334ccfec7d946a6d704d735d30afdbc
Maicon Renildo <maicon@lunes.io>
2022-08-29 20:25:46 +0000

    updated viewset and serializers

--- assets/viewset.py
+++ assets/viewset.py
@@ -1,5 +1,5 @@
+from rest_framework import viewsets,mixins
 from assets import serializers,models
-from rest_framework import viewsets
 
 
 class AssetViewSet(viewsets.ModelViewSet):
@@ -7,6 +7,16 @@ class AssetViewSet(viewsets.ModelViewSet):
     queryset = models.Asset.objects.all()
 
 
-class MonitoringViewSet(viewsets.ModelViewSet):
+class SimpleViewSet(
+    mixins.RetrieveModelMixin, 
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+    ):
+    pass
+
+
+class MonitoringViewSet(SimpleViewSet):
     serializer_class = serializers.MonitoringSerializer
     queryset = models.Monitoring.objects.all()
