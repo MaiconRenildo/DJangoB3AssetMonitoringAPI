1d9a5b55f7878d46cd0df57ede71e6c43abc1b42
Maicon Renildo <maicon@lunes.io>
2022-08-29 20:26:30 +0000

    updated utils functions

--- assets/utils.py
+++ assets/utils.py
@@ -1,66 +1,46 @@
-from assets import models
+from pydantic import BaseModel,Field
+from urllib.request import Request
 
-class AssetsRegister():
-
-    def __init__(self):    
-      self.not_exists = models.Asset.objects.exists() == False
-    
-
-    def get_assets(self):
-      import httpx
-      self.assets = httpx.get(
-        url="https://api-cotacao-b3.labdo.it/api/empresa",
-        timeout=60
-      ).json()
+class AssetMonitoringIn(BaseModel):
+    asset_id:str = Field(...,example="f7d49390-cdb7-4a44-8fa4-235bfdddf113")
+    upper_price_limit: float = Field(...,example= 50.0)
+    lower_price_limit:float = Field(...,example=60.00) 
+    id:str = Field(...,example="58d003ba-edb5-4ff6-a0e7-202a40aae494")
+    interval:int = Field(...,example=3600)
 
 
-    def insert(self):
-      for asset in self.assets:
-
-        codes = asset['cd_acao'].split(",")
-
-        for code in codes:
-
-          code = code.lstrip()
-              
-          if code != "":
-            models.Asset.objects.get_or_create(
-              code = code,
-              company_name = asset['nm_empresa'],
-              CNPJ = asset['vl_cnpj']
-            )
-
-      print("Dados dos ativos cadastrados com sucesso")
-
-
-def get_asset_cotation(code:str):
-    import httpx,os,dotenv
-    dotenv.load_dotenv(dotenv.find_dotenv())
-    response = httpx.get(
-        url="https://api.hgbrasil.com/finance/stock_price?key=" + os.getenv("HG_API_KEY") + "&symbol=" + code,
-        timeout=60
-    ).json()['results']
-
+def register_monitoring(monitoring_data:AssetMonitoringIn):
+    from assets.models import Monitoring,Asset
     try:
-        return response[code.upper()]['price']
+      Monitoring(
+        asset_id =Asset(id=monitoring_data.asset_id),
+        upper_price_limit=monitoring_data.upper_price_limit,
+        lower_price_limit=monitoring_data.lower_price_limit,
+        interval=monitoring_data.interval
+      ).save()
+      return True
     except:
-        raise Exception("Code note found")
+      return False
 
 
-def send_purchase_recommendation_email(asset_code:str,price:float,email:str):
-    from assets.email import send
+def get_monitoring_params(req:Request):
+    import ast
+    try:
+      body = dict(ast.literal_eval(req.body.decode("utf-8")))
+      return AssetMonitoringIn(
+        upper_price_limit=body['upper_price_limit'],
+        lower_price_limit=body['lower_price_limit'],
+        interval=body['interval'],
+        asset_id=body['asset_id'],
+        id=body['id'],
+      )
+    except:
+      return False
 
-    return send(
-        to=email,
-        subject="Recomendação de compra - " + asset_code,
-        msg="A cotação atual do ativo é de R$ " + str(price) + " . Conforme os parâmetros de monitoramento, sugerimos a compra." 
-    )
 
-
-def send_sale_recommendation_email(asset_code:str,price:float,email:str):
-    from assets.email import send
-    return send(
-        to=email,
-        subject="Recomendação de venda - " + asset_code,
-        msg="A cotação atual do ativo é de R$ " + str(price) + " . Conforme os parâmetros de monitoramento, sugerimos a venda." 
-    )
+def get_asset(asset_id):
+    from assets.models import Asset
+    try:
+      return Asset.objects.filter(id=asset_id).values()[0]
+    except:
+      return False
